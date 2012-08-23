c51ee411c3376227f4973785a30fc825be1f85b2
dzamkov <dzamkov@xiatek.org>
2012-08-23 16:19:33 -0400

    Added lua interface and serialization code

---
+++ Interface/Primitive.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using KopiLua;
+
+namespace Hailstone.Interface
+{
+    /// <summary>
+    /// A type interface for several primitive types.
+    /// </summary>
+    public class PrimitiveTypeInterface : ITypeInterface<double>, ITypeInterface<int>, ITypeInterface<string>
+    {
+        private PrimitiveTypeInterface()
+        {
+
+        }
+
+        /// <summary>
+        /// The only instance of this class.
+        /// </summary>
+        public static readonly PrimitiveTypeInterface Instance = new PrimitiveTypeInterface();
+
+        /// <summary>
+        /// Registers the primitive type interface.
+        /// </summary>
+        public static void Register()
+        {
+            TypeInterface.Set<double>(PrimitiveTypeInterface.Instance);
+            TypeInterface.Set<int>(PrimitiveTypeInterface.Instance);
+            TypeInterface.Set<string>(PrimitiveTypeInterface.Instance);
+        }
+
+        void ITypeInterface<double>.Push(Lua.lua_State State, double Object)
+        {
+            Lua.lua_pushnumber(State, Object);
+        }
+
+        double ITypeInterface<double>.To(Lua.lua_State State, int Index)
+        {
+            if (Lua.lua_isnumber(State, Index) > 0)
+                return Lua.lua_tonumber(State, Index);
+            else
+                throw new Exception(String.Format("Expected number (Got {0}).", Lua.lua_typename(State, Index).ToString()));
+        }
+
+        string ITypeInterface<double>.Create(double Object)
+        {
+            return Object.ToString(System.Globalization.CultureInfo.InvariantCulture);
+        }
+
+        bool ITypeInterface<double>.Mutate(string Name, double From, double To, List<string> Code)
+        {
+            if (From != To) Code.Add(Name + " = " + To.ToString(System.Globalization.CultureInfo.InvariantCulture));
+            return true;
+        }
+
+        void ITypeInterface<int>.Push(Lua.lua_State State, int Object)
+        {
+            Lua.lua_pushinteger(State, Object);
+        }
+
+        int ITypeInterface<int>.To(Lua.lua_State State, int Index)
+        {
+            if (Lua.lua_isnumber(State, Index) > 0)
+                return (int)Lua.lua_tonumber(State, Index);
+            else
+                throw new Exception(String.Format("Expected integer (Got {0}).", Lua.lua_typename(State, Index).ToString()));
+        }
+
+        string ITypeInterface<int>.Create(int Object)
+        {
+            return Object.ToString(System.Globalization.CultureInfo.InvariantCulture);
+        }
+
+        bool ITypeInterface<int>.Mutate(string Name, int From, int To, List<string> Code)
+        {
+            if (From != To) Code.Add(Name + " = " + To.ToString(System.Globalization.CultureInfo.InvariantCulture));
+            return true;
+        }
+
+        void ITypeInterface<string>.Push(Lua.lua_State State, string Object)
+        {
+            Lua.lua_pushstring(State, Object);
+        }
+
+        string ITypeInterface<string>.To(Lua.lua_State State, int Index)
+        {
+            if (Lua.lua_isstring(State, Index) > 0)
+                return Lua.lua_tostring(State, Index).ToString();
+            else
+                throw new Exception(String.Format("Expected string (Got {0}).", Lua.lua_typename(State, Index).ToString()));
+        }
+
+        string ITypeInterface<string>.Create(string Object)
+        {
+            return Object;
+        }
+
+        bool ITypeInterface<string>.Mutate(string Name, string From, string To, List<string> Code)
+        {
+            if (From != To) Code.Add(Name + " = " + To);
+            return true;
+        }
+    }
+}
---
+++ Interface/TypeInterface.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using KopiLua;
+
+namespace Hailstone.Interface
+{
+    /// <summary>
+    /// Contains methods that allow interfacing with an object of a certain type.
+    /// </summary>
+    public interface ITypeInterface<T>
+    {
+        /// <summary>
+        /// Pushes a Lua representation of the given object to the Lua stack.
+        /// </summary>
+        void Push(Lua.lua_State State, T Object);
+
+        /// <summary>
+        /// Converts the item at the given location on the stack to an object.
+        /// </summary>
+        T To(Lua.lua_State State, int Index);
+
+        /// <summary>
+        /// Gets the Lua code (in the global scope) needed to create the given object, or null
+        /// if not possible.
+        /// </summary>
+        string Create(T Object);
+
+        /// <summary>
+        /// Gets the Lua code (in the global scope) needed to mutate the object "From" (with the given name) to 
+        /// be equivalent to "To", or returns false if not possible.
+        /// </summary>
+        bool Mutate(string Name, T From, T To, List<string> Code);
+    }
+
+    /// <summary>
+    /// Stores type interfaces for various types.
+    /// </summary>
+    public static class TypeInterface
+    {
+        /// <summary>
+        /// Sets the interface for the given type.
+        /// </summary>
+        public static void Set<T>(ITypeInterface<T> Interface)
+        {
+            Set(typeof(T), new _Wrapper<T>(Interface));
+        }
+
+        /// <summary>
+        /// Sets the interface for the given type.
+        /// </summary>
+        public static void Set(Type Type, ITypeInterface<object> Interface)
+        {
+            _Interfaces[Type] = Interface;
+        }
+
+        /// <summary>
+        /// Gets the interface for the given type, or null if none exists.
+        /// </summary>
+        public static ITypeInterface<object> Get(Type Type)
+        {
+            ITypeInterface<object> i;
+            if (_Interfaces.TryGetValue(Type, out i))
+                return i;
+            else
+                return null;
+        }
+
+        /// <summary>
+        /// Gets the interface for the given type, or null if none exists.
+        /// </summary>
+        public static ITypeInterface<T> Get<T>()
+        {
+            ITypeInterface<object> i = Get(typeof(T));
+            if (i == null) return null;
+            _Wrapper<T> wrapper = i as _Wrapper<T>;
+            if (wrapper != null) return wrapper.Source;
+            return (i as ITypeInterface<T>);
+        }
+
+        /// <summary>
+        /// The known type interfaces. 
+        /// </summary>
+        private static readonly Dictionary<Type, ITypeInterface<object>> _Interfaces = 
+            new Dictionary<Type, ITypeInterface<object>>();
+
+        /// <summary>
+        /// A wrapper for a type interface that allows it to work on a general object type.
+        /// </summary>
+        private class _Wrapper<T> : ITypeInterface<object>
+        {
+            public _Wrapper(ITypeInterface<T> Source)
+            {
+                this.Source = Source;
+            }
+
+            /// <summary>
+            /// The source type interface for this wrapper.
+            /// </summary>
+            public readonly ITypeInterface<T> Source;
+
+            void ITypeInterface<object>.Push(Lua.lua_State State, object Object)
+            {
+                this.Source.Push(State, (T)Object);
+            }
+
+            object ITypeInterface<object>.To(Lua.lua_State State, int Index)
+            {
+                return (object)this.Source.To(State, Index);
+            }
+
+            string ITypeInterface<object>.Create(object Object)
+            {
+                return this.Source.Create((T)Object);
+            }
+
+            bool ITypeInterface<object>.Mutate(string Name, object From, object To, List<string> Code)
+            {
+                return this.Source.Mutate(Name, (T)From, (T)To, Code);
+            }
+        }
+    }
+}
